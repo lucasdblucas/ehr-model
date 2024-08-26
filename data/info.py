b912c6cc81b75e9e040013fe7f5bedf227ae3fdf
lucas_nitlab <lucas.daniel.bp@gmail.com>
2024-08-26 14:25:49 -0300

    firt commit - nitlab

---
+++ data/info.py
@@ -0,0 +1,7 @@
+
+class InfoData():
+    def __init__(self,
+        directory_path="F:/datasets/MIMIC-IV 3.0/physionet.org/files/mimiciv/3.0"
+    ) -> None:
+        
+        self.directory_path = directory_path
