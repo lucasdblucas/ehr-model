15a6d916ce288ceefb3ec45809982ba61092fb9d
Lucas <lucas.daniel.bp@gmail.com>
2024-09-01 17:26:01 -0300

    update - SuperPC

--- data/info.py
+++ data/info.py
@@ -1,7 +1,16 @@
 
+env_config_map: dict = {
+    "nitlab": {
+        "mimiciv": "F:/datasets/MIMIC-IV 3.0/physionet.org/files/mimiciv/3.0"
+    },
+    "superpc": {
+        "mimiciv": "C:/out_the_drive/datasets_in_use/MIMIC-IV-3.0/files/mimiciv/3.0"
+    }
+}
+
 class InfoData():
-    def __init__(self,
-        directory_path="F:/datasets/MIMIC-IV 3.0/physionet.org/files/mimiciv/3.0"
+    def __init__(self, 
+        env: str = "nitlab"
     ) -> None:
         
-        self.directory_path = directory_path
+        self.mimiciv_directory_path = env_config_map["superpc"]["mimiciv"]
