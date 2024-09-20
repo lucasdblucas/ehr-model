4e29720c756a6c867c790b74ba614b05f8bd49b9
Lucas <lucas.daniel.bp@gmail.com>
2024-09-19 21:57:30 -0300

    update - supercomputador

--- data/info.py
+++ data/info.py
@@ -4,7 +4,8 @@ env_config_map: dict = {
         "mimiciv": "F:/datasets/MIMIC-IV 3.0/physionet.org/files/mimiciv/3.0"
     },
     "superpc": {
-        "mimiciv": "C:/out_the_drive/datasets_in_use/MIMIC-IV-3.0/files/mimiciv/3.0"
+        "mimiciv": "C:/out_the_drive/datasets_in_use/MIMIC-IV-3.0/files/mimiciv/3.0",
+        "icd10icd9": "C:/out_the_drive/datasets_in_use/MIMIC-IV-3.0/icd9toicd10cmgem.csv"
     }
 }
 
@@ -14,3 +15,4 @@ class InfoData():
     ) -> None:
         
         self.mimiciv_directory_path = env_config_map["superpc"]["mimiciv"]
+        self.converter_csv_path = env_config_map["superpc"]["icd10icd9"]
